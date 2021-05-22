a6d58dd9dbb9d529b02683f03f9ccce31791d565
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-05-22 19:33:35 +0530

    Added media queries , opacity for about section and minified images.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
 const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
+const opacity = document.querySelectorAll(".opacity");
+const border = document.querySelector(".border");
 
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
@@ -21,9 +23,17 @@ window.addEventListener("scroll",()=>{
         element.style.transform = `translateY(${scroll * speed}px)`;
     })
 
+    opacity.forEach(element => {
+        element.style.opacity = -1 + scroll / (header_height / 2 );
+    })
+
+    border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`
+
     big_title.style.opacity = - scroll / (header_height / 2 ) + 1; //Making Title Disappear 
     shadow.style.height = `${scroll * 0.5 + 300 }px`;
 
     content.style.transform =`translateY(${scroll / (section_height + sectionY.top ) * 50 - 50}px)`;
     image_container.style.transform =`translateY(${scroll / (section_height + sectionY.top ) * -50 + 50}px)`;
+
+
 })
